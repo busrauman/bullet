91bb88ae661e3276cb6041f67bd736b9c036f172
GitHub Action <action@github.com>
2021-07-04 15:45:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/busrauman/bullet/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/busrauman/bullet/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.busrauman.bullet.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.busrauman.bullet.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/busrauman/bullet/services/MyProjectService.kt
+++ src/main/kotlin/com/github/busrauman/bullet/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.busrauman.bullet.services
 
+import com.github.busrauman.bullet.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
